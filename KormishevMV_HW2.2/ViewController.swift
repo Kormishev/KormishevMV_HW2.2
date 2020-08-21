09734c41be9681f67b477ec10de3fb70ea27c1bb
Kormishev <kormishev@bk.ru>
2020-08-21 21:35:23 +0300

    Initial Commit

---
+++ KormishevMV_HW2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KormishevMV_HW2.2
+//
+//  Created by Михаил on 21.08.2020.
+//  Copyright © 2020 kormishev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
