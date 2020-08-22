bc6cee718dbac5f785b20d3a5c78124bd9d14992
Kormishev <kormishev@bk.ru>
2020-08-22 14:08:24 +0300

    1. Создан базовый функционал.

--- KormishevMV_HW2.2/ViewController.swift
+++ KormishevMV_HW2.2/ViewController.swift
@@ -1,20 +1,44 @@
-//
-//  ViewController.swift
-//  KormishevMV_HW2.2
-//
-//  Created by Михаил on 21.08.2020.
-//  Copyright © 2020 kormishev. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var screenView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        screenView.layer.cornerRadius = 15
+        changeColorScreenView()
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
-
-
+    
+    private func changeColorScreenView() {
+        screenView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0)
+    }
+    
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        changeColorScreenView()
+    }
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        changeColorScreenView()
+    }
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeColorScreenView()
+    }
 }
 
